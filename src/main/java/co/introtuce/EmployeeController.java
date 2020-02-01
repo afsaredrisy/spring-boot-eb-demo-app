6e58f3b1762652004cf2e6c6f3d183e92702b252
afsaredrisy <afsaredrisy@gmail.com>
2020-02-01 13:07:49 +0530

    initial commit

---
+++ src/main/java/co/introtuce/EmployeeController.java
@@ -0,0 +1,41 @@
+package co.introtuce;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(path = "/myreq")
+public class EmployeeController {
+	 @Autowired
+	    private EmployeeDAO employeeDao;
+	     
+	    @GetMapping(path="/", produces = "application/json")
+	    public Employees getEmployees() 
+	    {
+	        return employeeDao.getAllEmployees();
+	    }
+	     
+	    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+	    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) 
+	    {
+	        Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
+	        employee.setId(id);
+	         
+	        employeeDao.addEmployee(employee);
+	         
+	        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+	                                    .path("/{id}")
+	                                    .buildAndExpand(employee.getId())
+	                                    .toUri();
+	         
+	        return ResponseEntity.created(location).build();
+	    }
+}
